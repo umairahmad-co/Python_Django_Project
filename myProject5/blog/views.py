3a6e0a7644521dbde144f370b30bc368e287641b
umairahmad-co <umairahmad.co@gmail.com>
2026-07-30 00:47:10 +0500

    update

---
+++ myProject5/blog/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from datetime import datetime
+
+# Create your views here.
+
+class User:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+def home(request):
+    context = {
+        'name': "Umair Ahmad",
+        'age': 25,
+        'skills': ['Python', 'Django', 'JavaScript'],
+        'user': User('Ahmad', 30),
+        "blog": {
+            "title": "Django Template Intro",
+            "content": "<b>This is Blod.</b>",
+            "created_at": datetime(2026, 6, 15, 10, 30)
+        },
+        "empty_value": None
+    }
+    return render(request, 'home.html', context)
